cc19394cd1d64037ba88cd12b050081cc4910c92
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2024-02-12 22:39:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function nextPalindrome(num) {
   //your JS code here.
-}
+	  function isPalindrome(n) {
+    return n.toString() === n.toString().split('').reverse().join('');
+  }
 
+  // Start checking from the next number
+  let nextNum = num + 1;
+  while (true) {
+    if (isPalindrome(nextNum)) {
+      return nextNum;
+    }
+    nextNum++;
+  }
+}
+ 
 const input = prompt("Enter a palindrome number");
 alert(nextPalindrome(input));
